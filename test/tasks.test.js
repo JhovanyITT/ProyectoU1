b70c884c67d945c0bca75f9b8652ce474c2548b3
CrissCross20 <165857522+CrissCross20@users.noreply.github.com>
2024-10-29 21:16:41 -0700

    Ahora si ya uwu

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -14,174 +14,174 @@ let oldTask;
 
 //#region Escenarios de prueba del CRUD en MongoDB
 
-// //Prueba #1: GET All TASK - MongoDB
-// describe('GET ALL TASKS /api/mongodb/task', () => {
+//Prueba #1: GET All TASK - MongoDB
+describe('GET ALL TASKS /api/mongodb/task', () => {
 
-//     // Como primer paso nos autenticamos para poder realizar la consulta
-//     before(async () => {
-//         const res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
-//         token = res.body.token;
-//         expect(token).to.be.a('string');
-//     });
+    // Como primer paso nos autenticamos para poder realizar la consulta
+    before(async () => {
+        const res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+    });
 
-//     //Seguidamente con el token valido realizamos la consulta
-//     it('1, Deberia devolver todas las tareas con estatus 200 cuando hay tareas en MongoDB', async () => {
-//         const res = await request(app).get('/api/mongodb/task').set('Authorization', `Bearer ${token}`);
-//         expect(res.status).to.equal(200);
-//         expect(res.body).to.be.an('array');
+    //Seguidamente con el token valido realizamos la consulta
+    it('1, Deberia devolver todas las tareas con estatus 200 cuando hay tareas en MongoDB', async () => {
+        const res = await request(app).get('/api/mongodb/task').set('Authorization', `Bearer ${token}`);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
 
-//         res.body.forEach(task => {
-//             expect(task).to.include.keys('_id', 'fk_user_id', 'title', 'body', 'creation_date', 'completed');
-//             expect(task._id).to.be.a('string');
-//             expect(task.fk_user_id).to.be.a('string');
-//             expect(task.title).to.be.a('string')
-//             expect(task.body).to.be.a('string');
-//             expect(task.completed).to.be.oneOf([true, false]);
-//         });
+        res.body.forEach(task => {
+            expect(task).to.include.keys('_id', 'fk_user_id', 'title', 'body', 'creation_date', 'completed');
+            expect(task._id).to.be.a('string');
+            expect(task.fk_user_id).to.be.a('string');
+            expect(task.title).to.be.a('string')
+            expect(task.body).to.be.a('string');
+            expect(task.completed).to.be.oneOf([true, false]);
+        });
 
-//     });
-// });
+    });
+});
 
-// //Prueba #2: GET TASK BY ID - MongoDB
-// describe('GET TASK BY ID /api/mongodb/task/:id', () => {
+//Prueba #2: GET TASK BY ID - MongoDB
+describe('GET TASK BY ID /api/mongodb/task/:id', () => {
 
-//     before(async () => {
-//         let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
-//         token = res.body.token;
-//         expect(token).to.be.a('string');
-//         res = await request(app).post('/api/mongodb/task').send({ fk_user_id: 'admin', title: 'Tarea  de Prueba', body: 'Esto es una tarea de Testeo :0', completed: false }).set('Authorization', `Bearer ${token}`);
-//         idTask = res.body.newTask._id;
-//         dateTask = res.body.newTask.creation_date;
-//         expect(idTask).to.be.a('string');
-//         expect(dateTask).to.be.a('string');
-//     });
+    before(async () => {
+        let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+        res = await request(app).post('/api/mongodb/task').send({ fk_user_id: 'admin', title: 'Tarea  de Prueba', body: 'Esto es una tarea de Testeo :0', completed: false }).set('Authorization', `Bearer ${token}`);
+        idTask = res.body.newTask._id;
+        dateTask = res.body.newTask.creation_date;
+        expect(idTask).to.be.a('string');
+        expect(dateTask).to.be.a('string');
+    });
 
-//     it('2, Deberia devolver una tarea con estatus 200 cuando la tarea corresponde con el ID en MongoDB', async () => {
-//         const task = {
-//             _id: idTask,
-//             fk_user_id: 'admin',
-//             title: 'Tarea  de Prueba', 
-//             body: 'Esto es una tarea de Testeo :0',
-//             creation_date: dateTask,
-//             completed: false
-//         };
-//         const res = await request(app).get(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
-//         expect(res.status).to.equal(200);
-//         expect(res.body).to.include.keys('_id', 'fk_user_id', 'title', 'body', 'creation_date', 'completed');
-//         expect(res.body).to.deep.equal(task);
-//     });
-// });
+    it('2, Deberia devolver una tarea con estatus 200 cuando la tarea corresponde con el ID en MongoDB', async () => {
+        const task = {
+            _id: idTask,
+            fk_user_id: 'admin',
+            title: 'Tarea  de Prueba', 
+            body: 'Esto es una tarea de Testeo :0',
+            creation_date: dateTask,
+            completed: false
+        };
+        const res = await request(app).get(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.include.keys('_id', 'fk_user_id', 'title', 'body', 'creation_date', 'completed');
+        expect(res.body).to.deep.equal(task);
+    });
+});
 
-// //Prueba #3: GET ALL USER TASKS - MongoDB
-// describe('GET ALL USER TASKS /api/mongodb/task/user/:id', () => {
+//Prueba #3: GET ALL USER TASKS - MongoDB
+describe('GET ALL USER TASKS /api/mongodb/task/user/:id', () => {
 
-//     // Como primer paso nos autenticamos para poder realizar la consulta
-//     before(async () => {
-//         const res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
-//         token = res.body.token;
-//         expect(token).to.be.a('string');
-//     });
+    // Como primer paso nos autenticamos para poder realizar la consulta
+    before(async () => {
+        const res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+    });
 
-//     //Seguidamente con el token valido realizamos la consulta
-//     it('3, Deberia devolver todas las tareas con estatus 200 cuando hay tareas del usuario indicado en MongoDB', async () => {
-//         const res = await request(app).get('/api/mongodb/task/user/admin').set('Authorization', `Bearer ${token}`);
-//         expect(res.status).to.equal(200);
-//         expect(res.body).to.be.an('array');
+    //Seguidamente con el token valido realizamos la consulta
+    it('3, Deberia devolver todas las tareas con estatus 200 cuando hay tareas del usuario indicado en MongoDB', async () => {
+        const res = await request(app).get('/api/mongodb/task/user/admin').set('Authorization', `Bearer ${token}`);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
 
-//         res.body.forEach(task => {
-//             expect(task).to.include.keys('_id', 'fk_user_id', 'title', 'body', 'creation_date', 'completed');
-//             expect(task._id).to.be.a('string');
-//             expect(task.fk_user_id).to.be.a('string').to.equal('admin');
-//             expect(task.title).to.be.a('string')
-//             expect(task.body).to.be.a('string');
-//             expect(task.completed).to.be.oneOf([true, false]);
-//         });
+        res.body.forEach(task => {
+            expect(task).to.include.keys('_id', 'fk_user_id', 'title', 'body', 'creation_date', 'completed');
+            expect(task._id).to.be.a('string');
+            expect(task.fk_user_id).to.be.a('string').to.equal('admin');
+            expect(task.title).to.be.a('string')
+            expect(task.body).to.be.a('string');
+            expect(task.completed).to.be.oneOf([true, false]);
+        });
 
-//     });
-// });
+    });
+});
 
-// //Prueba #4: CREATE NEW TASK - MongoDB
-// describe('CREATE NEW TASK /api/mongodb/task', () => {
+//Prueba #4: CREATE NEW TASK - MongoDB
+describe('CREATE NEW TASK /api/mongodb/task', () => {
 
-//     before(async () => {
-//         let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
-//         token = res.body.token;
-//         expect(token).to.be.a('string');
-//     });
+    before(async () => {
+        let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+    });
 
-//     it('4, Deberia devolver la tarea creada MongoDB con estatus 201', async () => {
-//         const res = await request(app).post('/api/mongodb/task').send({ fk_user_id: 'admin', title: 'Tarea  de Prueba 2', body: 'Esto es una tarea de Testeo Ditto', completed: false }).set('Authorization', `Bearer ${token}`);
-//         idTask = res.body.newTask._id;
-//         dateTask = res.body.newTask.creation_date;
-//         const task = {
-//             serverResult: {
-// 		        acknowledged: true,
-// 		        insertedId: idTask
-// 	        },
-//             newTask:{
-//                 _id: idTask,
-//                 fk_user_id: 'admin',
-//                 title: 'Tarea  de Prueba 2', 
-//                 body: 'Esto es una tarea de Testeo Ditto',
-//                 creation_date: dateTask,
-//                 completed: false
-//             }
-//         };
-//         expect(res.status).to.equal(201);
-//         expect(res.body).to.deep.equal(task);
-//         expect(res.body.newTask.completed).to.be.oneOf([true, false]);
-//     });
-// });
+    it('4, Deberia devolver la tarea creada MongoDB con estatus 201', async () => {
+        const res = await request(app).post('/api/mongodb/task').send({ fk_user_id: 'admin', title: 'Tarea  de Prueba 2', body: 'Esto es una tarea de Testeo Ditto', completed: false }).set('Authorization', `Bearer ${token}`);
+        idTask = res.body.newTask._id;
+        dateTask = res.body.newTask.creation_date;
+        const task = {
+            serverResult: {
+		        acknowledged: true,
+		        insertedId: idTask
+	        },
+            newTask:{
+                _id: idTask,
+                fk_user_id: 'admin',
+                title: 'Tarea  de Prueba 2', 
+                body: 'Esto es una tarea de Testeo Ditto',
+                creation_date: dateTask,
+                completed: false
+            }
+        };
+        expect(res.status).to.equal(201);
+        expect(res.body).to.deep.equal(task);
+        expect(res.body.newTask.completed).to.be.oneOf([true, false]);
+    });
+});
 
-// //Prueba #5: EDIT TASK - MongoDB
-// describe('EDIT TASK /api/mongodb/task/:id', () => {
+//Prueba #5: EDIT TASK - MongoDB
+describe('EDIT TASK /api/mongodb/task/:id', () => {
 
-//     before(async () => {
-//         let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
-//         token = res.body.token;
-//         expect(token).to.be.a('string');
-//         res = await request(app).get(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
-//         oldTask = res.body;
-//     });
+    before(async () => {
+        let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+        res = await request(app).get(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
+        oldTask = res.body;
+    });
 
-//     it('5, Deberia devolver la tarea actualizada y la tarea antes de la modificación con estatus 201', async () => {
-//         const res = await request(app).put(`/api/mongodb/task/${idTask}`).send({ title: 'Tarea Actualizada', body: 'Esto es una tarea Actualizada', completed: true }).set('Authorization', `Bearer ${token}`);
-//         const task = {
-//             oldTask: 
-//                 oldTask,
-//             editedTask: {
-//                 fk_user_id: 'admin',
-//                 title: 'Tarea Actualizada',
-//                 body: 'Esto es una tarea Actualizada',
-//                 creation_date: oldTask.creation_date,
-//                 completed: true
-// 	        }
-//         };
-//         expect(res.status).to.equal(201);
-//         expect(res.body.editedTask.title).to.be.a('string');
-//         expect(res.body.editedTask.body).to.be.a('string');
-//         expect(res.body.editedTask.completed).to.be.oneOf([true, false]);
-//         expect(res.body).to.deep.equal(task);
-//     });
-// });
+    it('5, Deberia devolver la tarea actualizada y la tarea antes de la modificación con estatus 201', async () => {
+        const res = await request(app).put(`/api/mongodb/task/${idTask}`).send({ title: 'Tarea Actualizada', body: 'Esto es una tarea Actualizada', completed: true }).set('Authorization', `Bearer ${token}`);
+        const task = {
+            oldTask: 
+                oldTask,
+            editedTask: {
+                fk_user_id: 'admin',
+                title: 'Tarea Actualizada',
+                body: 'Esto es una tarea Actualizada',
+                creation_date: oldTask.creation_date,
+                completed: true
+	        }
+        };
+        expect(res.status).to.equal(201);
+        expect(res.body.editedTask.title).to.be.a('string');
+        expect(res.body.editedTask.body).to.be.a('string');
+        expect(res.body.editedTask.completed).to.be.oneOf([true, false]);
+        expect(res.body).to.deep.equal(task);
+    });
+});
 
-// //Prueba #6: DELETE TASK BY ID - MongoDB
-// describe('DELETE TASK /api/mongodb/task/:id', () => {
+//Prueba #6: DELETE TASK BY ID - MongoDB
+describe('DELETE TASK /api/mongodb/task/:id', () => {
 
-//     before(async () => {
-//         let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
-//         token = res.body.token;
-//         expect(token).to.be.a('string');
-//         res = await request(app).get(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
-//         oldTask = res.body;
-//     });
+    before(async () => {
+        let res = await request(app).post('/api/auth/login').send({ username: 'admin', password: '12345' });
+        token = res.body.token;
+        expect(token).to.be.a('string');
+        res = await request(app).get(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
+        oldTask = res.body;
+    });
 
-//     it('6, Deberia devolver la tarea eliminada con estatus 200', async () => {
-//         const res = await request(app).delete(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
-//         expect(res.status).to.equal(200);
-//         expect(res.body).to.deep.equal(oldTask);
-//     });
-// });
+    it('6, Deberia devolver la tarea eliminada con estatus 200', async () => {
+        const res = await request(app).delete(`/api/mongodb/task/${idTask}`).set('Authorization', `Bearer ${token}`);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal(oldTask);
+    });
+});
 
 //#endregion
 
